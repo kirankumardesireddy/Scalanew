f0df2bc8a6be3a47bb474a833fe11085968dff56
kirankumardesireddy <51425736+kirankumardesireddy@users.noreply.github.com>
2019-10-21 18:11:54 -0700

    Create testfile.scala

---
+++ src/testfile.scala
@@ -0,0 +1,15 @@
+object hello {
+  
+  def main (args: Array[String]) {
+    println ("hello")
+    
+    val colors = Map("red" -> "#FF0000", "azure" -> "#F0FFFF")
+
+     println(colors)
+     
+      println(colors("red"))
+    
+  }
+  
+  
+}
